64c8e6a604fb54d27d0af7203c26bdc257e04f29
Vu Vo <truongvukhmt05@gmail.com>
2018-12-18 22:10:13 +0700

    create chef show action with tests

---
+++ test/integration/chefs_show_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class ChefsShowTest < ActionDispatch::IntegrationTest
+
+	def setup
+		@chef = Chef.create!(chefname: "VuVT 01", email: "truongvukhmt0@gmail.com", password: "password", password_confirmation: "password")
+		@recipe = Recipe.create!(name: "vegetables saute", description: "good vegetables saute, add oil", chef: @chef)
+		@recipe2 = @chef.recipes.build(name: "chicken saute", description: "really good chicken saute")
+		@recipe2.save
+	end
+
+	test "should get chef show" do 
+		get chef_path(@chef)
+		assert_template "chefs/show"
+		assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
+  	assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name 
+  	assert_match @recipe.description, response.body
+  	assert_match @recipe2.description, response.body
+	end
+end
