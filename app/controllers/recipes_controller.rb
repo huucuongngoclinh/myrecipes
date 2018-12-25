23ce68f26534d6bce2aca56a30c306a17629687b
Vu Vo <truongvukhmt05@gmail.com>
2018-12-25 23:16:35 +0700

    complete ingredient resources

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -53,6 +53,6 @@ class RecipesController < ApplicationController
 		end
 
 		def recipe_params
-			params.require(:recipe).permit(:name, :description)
+			params.require(:recipe).permit(:name, :description, ingredient_ids: [])
 		end
 end
