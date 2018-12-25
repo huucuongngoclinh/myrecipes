c2cc7e9e2a5efa70c98b45068eb58b13f14908c2
Vu Vo <truongvukhmt05@gmail.com>
2018-12-25 19:27:18 +0700

    add comment and association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_24_144118) do
+ActiveRecord::Schema.define(version: 2018_12_24_150718) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2018_12_24_144118) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.integer "chef_id"
+    t.integer "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "ingredients", force: :cascade do |t|
